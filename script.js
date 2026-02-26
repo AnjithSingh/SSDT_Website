5bcde80c61adc012bba08e856469fc99be3ea813
AnjithSingh <anjithsingh1t@gmail.com>
2026-02-26 20:48:28 +0530

    Changes

--- script.js
+++ script.js
@@ -5,17 +5,13 @@ function sendWhatsApp(e) {
   const cls = document.getElementById("class").value;
   const parent = document.getElementById("parent").value;
   const phone = document.getElementById("phone").value;
-  const school = document.getElementById("school").value;
-  const message = document.getElementById("message").value;
 
   const text =
     `Admission Enquiry:%0A` +
-    `Student Name: ${student}%0A` +
+    `Student: ${student}%0A` +
     `Class: ${cls}%0A` +
-    `Parent Name: ${parent}%0A` +
-    `Mobile: ${phone}%0A` +
-    `School/College: ${school}%0A` +
-    `Message: ${message}`;
+    `Parent: ${parent}%0A` +
+    `Phone: ${phone}`;
 
-  window.open(`https://wa.me/918340011651?text=${text}`, "_blank");
+  window.open(`https://wa.me/916303423475?text=${text}`, "_blank");
 }
